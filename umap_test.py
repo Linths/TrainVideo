b0dd5613e562c768fb54c24f328d153a56857b04
Lindsay Kempen <l.a.o.kempen@student.utwente.nl>
2019-05-21 16:11:44 +0200

    Try out umap on MNIST data

---
+++ umap_test.py
@@ -0,0 +1,18 @@
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Dimension reduction and clustering libraries
+import umap
+import hdbscan
+import sklearn.cluster as cluster
+from sklearn.metrics import adjusted_rand_score, adjusted_mutual_info_score
+
+sns.set(style='white', rc={'figure.figsize':(10,8)})
+mnist = load_digits()
+# mnist = fetch_openml('mnist_784')
+standard_embedding = umap.UMAP(random_state=42).fit_transform(mnist.data)
+plt.scatter(standard_embedding[:, 0], standard_embedding[:, 1], c=mnist.target, s=0.1, cmap='Spectral');
+plt.show()
