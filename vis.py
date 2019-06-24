4d9ee4375e6e33bdc2a59a46280f6ec9ff65c3fe
Michiexb <mdj.peters@gmail.com>
2019-06-24 10:40:31 +0200

    Add vis.py documentation

--- vis.py
+++ vis.py
@@ -28,8 +28,9 @@ from torchvision import datasets
 class Visualization():
     alpha = 0.6
     colours = [[0, 0, 1], [1, 0, 0], [0.3, 1, 0], [1, 0, 1], [0, 1, 1], [1, 0.8, 0], [1, 0.9, 0.9], [0.6, 0.3, 1], [0.4, 1, 0.7], [1, 0.4, 0]]
-    # blue, red, green, pink, cyan, yellow, white, violet, teal, orange
-    # Class instances below will be initalized in init function
+    # colours: blue, red, green, magenta, cyan, yellow, salmon, violet, turquoise, orange
+    
+    # class instances below will be initalized in init function
     newcmp = None
     barcmp = None
     plot_image = []
@@ -44,22 +45,30 @@ class Visualization():
         self.TEST_PRED = TEST_PRED
         self.VIS_ACC = VIS_ACC
         self.TEST_ACC = TEST_ACC
-
+        
+        Visualization.plot_image = []
+        
+        # make two RGBA versions of the colour list and turn them into colormaps
         colours_long = copy.deepcopy(Visualization.colours)
         for col in colours_long:
             col.append(1)
         Visualization.newcmp = ListedColormap(colours_long)
+        colours_alpha = copy.deepcopy(Visualization.colours)
+        for col in colours_alpha:
+            col.append((1+alpha)/2)
+        Visualization.barcmp = ListedColormap(colours_alpha)
 
-        Visualization.plot_image = []
+        # start the accuracy plots at (0,0)
         self.VIS_ACC.append((0,0))
         self.TEST_ACC.append((0,0))
 
     def make_label_vis(self, output_dir, epochs_passed):
-        #make test vis with images
-        def getImage(image):
-            return OffsetImage(image)
+        # make a visualisation of the neural network's training process with 3 subplots.
+        # 1. a visualisation of the test data with images as plot points and a colorbar as legend
+        # 2. a visualisation of the training data
+        # 3. a visualisation of the accuracy of the test and training data
 
-        #setup umap dimensionality reduction
+        # set up umap dimensionality reduction
         fit = umap.UMAP(random_state=np.random.seed(42), n_neighbors=n_neigh)
         all_data = np.concatenate((self.VIS_DATA, self.TEST_DATA), axis=0)
         trans = fit.fit(all_data)
@@ -75,29 +84,28 @@ class Visualization():
         big_x = gridspec.GridSpec(2,4)
         big_x.update(wspace=0.3, hspace=0.5)
 
-        # big plot (test data)
+        # set up sub plot 1 (test data)
         ax = plt.subplot(big_x[:,:3])
         ax.title.set_text("Images for testing")
         ax.scatter(x, y, c=self.TEST_PRED, s=1, cmap=Visualization.newcmp)
         plt.setp(ax, xticks=[], yticks=[])
         for x0, y0, img in zip(x, y, Visualization.plot_image):
-            ab = AnnotationBbox(getImage(img), (x0, y0), frameon=False)
+            ab = AnnotationBbox(OffsetImage(img), (x0, y0), frameon=False)
             ax.add_artist(ab)
         sm = plt.cm.ScalarMappable(cmap=Visualization.barcmp)
         sm._A = []
         cb = plt.colorbar(sm)
-        #cb.set_label('predicted class', labelpad=-10, y=1, rotation=0)
         cb.ax.set_title('Predicted class')
         cb.set_ticks(np.arange(0.05,1.05,step=0.1))
         cb.set_ticklabels(self.classes)
 
-        # sub plot 2 (train data)
+        # set up sub plot 2 (train data)
         ax2 = plt.subplot(big_x[0,3])
         ax2.title.set_text("Images for training")
         ax2.scatter(train_x, train_y, c=self.VIS_PRED, s=1, cmap=Visualization.newcmp)
         plt.setp(ax2, xticks=[], yticks=[])
 
-        # sub plot 3 (accuracy)
+        # set up sub plot 3 (accuracy)
         ax4 = plt.subplot(big_x[1,3])
         ax4.title.set_text("Accuracy of the neural network")
         ax4.plot(*zip(*self.VIS_ACC), label='train set')
@@ -106,15 +114,17 @@ class Visualization():
         plt.xticks(np.arange(0,num_epochs+1,step=2))
         plt.yticks(np.arange(0,1.1,step=0.1))
 
-        # general info
+        # set up figure information
         fig.set_size_inches(w=28, h=16)
         plt.suptitle(f"The neural network's activaty when given sketch images, after training for {epochs_passed} epochs", fontsize=18)
-        #plt.title(f"Activations of all 100 neurons in the CNN's first fully connected layer, reduced to 2 dimensions using UMAP")
         plt.figtext(.5,.95,"Activations of all 100 neurons in the CNN's first fully connected layer, reduced to 2 dimensions using UMAP", fontsize=14, ha='center')
+        
+        # set up output information
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
         fig.savefig(f"{output_dir}/after epoch {epochs_passed:02d} of {num_epochs} (#c={num_classes}, bs={batch_size}, lr={learning_rate}).png")
 
+        # checking if it should print the figure
         if print_every_vis:
             plt.show()
         else:
@@ -123,6 +133,8 @@ class Visualization():
             plt.close()
 
     def add_class_colour(self, image, label):
+        # load the image and add a mask in the colour of the corresponding label, with transparency alpha.
+        # then resize the image to make it an appropriate plot point size and append it to the image list.
         img = img_as_float(image)[0]
         rows, cols = img.shape
         color_mask = np.zeros((rows, cols, 3))
@@ -138,6 +150,7 @@ class Visualization():
         Visualization.plot_image.append(img_small)
 
     def clear_after_epoch(self):
+        # clear all per-epoch-data containing lists
         self.VIS_DATA.clear()
         self.VIS_TARGET.clear()
         self.VIS_PRED.clear()
@@ -146,25 +159,22 @@ class Visualization():
         self.TEST_PRED.clear()
         Visualization.plot_image.clear()
 
-# Show random images of the train set
 def show_images(train_loader, classes):
-    # Get some random training images
+    # get some random images of the train set, show them and print first four labels
     dataiter = iter(train_loader)
     images, labels = dataiter.next()
-    # Show images
     imshow(torchvision.utils.make_grid(images))
-    # Print labels
     print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
-# Show an image
 def imshow(img):
-    img = img / 2 + 0.5;    # Unnormalize
+    # show an image
+    img = img / 2 + 0.5;    # unnormalize
     npimg = img.numpy();
     plt.imshow(np.transpose(npimg, (1,2,0)))
     plt.show()
 
-# Make a plot of the train accuracy
 def plot_results(loss_list, acc_list):
+    # make a plot of the train accuracy and loss
     p = figure(y_axis_label='Loss', width=850, y_range=(0, 1), title=f'Performance of sketches CNN [#classes = {num_classes}, batch size = {batch_size}, #epochs = {num_epochs}, learning rate = {learning_rate}]')
     p.extra_y_ranges = {'Accuracy': Range1d(start=0, end=100)}
     p.add_layout(LinearAxis(y_range_name='Accuracy', axis_label='Accuracy (%)'), 'right')
